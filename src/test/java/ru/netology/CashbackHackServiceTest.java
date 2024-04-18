d5d051d070e8d27c2de8be40f49b30d1453786dd
netotest <stepanmokan@ya.ru>
2024-04-18 18:59:29 +0300

    JunitBranchCreated

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -3,28 +3,28 @@ package ru.netology;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testBoundaryValuesOne() {
-        org.testng.Assert.assertEquals(service.remain(0), 1000);
+        org.junit.Assert.assertEquals(service.remain(0), 1000);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testBoundaryTwo() {
-        org.testng.Assert.assertEquals(service.remain(1), 999);
+        org.junit.Assert.assertEquals(service.remain(1), 999);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testBoundaryThree() {
-        org.testng.Assert.assertEquals(service.remain(999), 1);
+        org.junit.Assert.assertEquals(service.remain(999), 1);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testBoundaryFour() {
-        org.testng.Assert.assertEquals(service.remain(1000), 0);
+        org.junit.Assert.assertEquals(service.remain(1000), 0);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testBoundaryFive() {
-        org.testng.Assert.assertEquals(service.remain(1001), 999);
+        org.junit.Assert.assertEquals(service.remain(1001), 999);
     }
 }
